f305f09e290bfaf8fd06bbad37d9e16b4a74aee9
TanHM <tan.hm1211@gmail.com>
2022-05-08 23:38:16 +0700

    rm log

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # DoMaSystem
 
 Document: https://1drv.ms/w/s!AntOWg__OP8QkRfOVVwc1oSYQ2SY?e=aGbUrq
-https://husteduvn-my.sharepoint.com/:w:/g/personal/linh_ntt176802_sis_hust_edu_vn/EdfNAKu131RAnfvRcaXtLxYBUWMEhsCJ9YWYMxZkxwxwlA?rtime=wUUUJU332Ug
+https://husteduvn-my.sharepoint.com/:w:/g/personal/linh_ntt176802_sis_hust_edu_vn/EfpTPcIN6flOtioGoLZuc7AB8l2GEsX4gCHcUseP7h-hNA?e=RqV95h
--- frontend/src/config/backend.jsx
+++ frontend/src/config/backend.jsx
@@ -7,16 +7,18 @@ export const USER_GET_CURRENT_API = BACKEND_URL + '/user/me';
 export const USER_GET_BY_ID_API = BACKEND_URL + '/user/id/{id}';
 
 
-export const makeRequest = async (method, url, token) => {
-    const requestOptions = {
-      method: method,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + token,
-      },
-    };
-    // console.log(token);
-  
-    return fetch(url, requestOptions);
+export const makeRequest = async (method, url, token, body=null, contentType="application/json", delete_contentType=false) => {
+  let requestOptions = {
+    method: method,
+    headers: {
+      "Content-Type": contentType,
+      Authorization: "Bearer " + token,
+    },
   };
+  if (body !== null) requestOptions.body = body;
+  if (delete_contentType) delete requestOptions.headers["Content-Type"];
+  // console.log(token);
+
+  return fetch(url, requestOptions);
+};
   
