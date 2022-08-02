5940a3927df7577a996e109757555b1b07e4b011
TanHM <tan.hm1211@gmail.com>
2022-08-02 20:07:49 +0700

    add environemt variable

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
 # uvicorn main:app --port 3009 --reload
 # bin/rm ./log/gunicorn.pid
 . ./venv/bin/activate && \
-gunicorn -c ./gunicorn_config.py main:app
+DMS_FRONT_END_URL=http://localhost:3008 gunicorn -c ./gunicorn_config.py main:app
